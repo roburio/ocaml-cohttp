681e8f641347721cd37d51c991a37bccc1010ea0
Hannes Mehnert <hannes@mehnert.org>
2020-01-20 18:09:41 +0100

    .

--- cohttp-mirage/src/server_with_conduit.ml
+++ cohttp-mirage/src/server_with_conduit.ml
@@ -4,7 +4,7 @@ include Make.Server(Conduit_mirage.Flow)
 let connect t =
   let listen s f =
     Conduit_mirage.listen t s (fun ip ->
-        Printf.printf "cohttp mirage server with conduit connection from %s\n"
+        Printf.printf "cohttp mirage server with conduit connection from %s\n%!"
           (Ipaddr.V4.to_string ip);
         listen f)
   in
