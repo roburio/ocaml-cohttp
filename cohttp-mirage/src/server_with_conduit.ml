940e9e4b7df515ed5a6ba342957f24d18bc997c3
Hannes Mehnert <hannes@mehnert.org>
2020-01-20 18:04:46 +0100

    adapt to conduit changes

--- cohttp-mirage/src/server_with_conduit.ml
+++ cohttp-mirage/src/server_with_conduit.ml
@@ -2,5 +2,10 @@
 include Make.Server(Conduit_mirage.Flow)
 
 let connect t =
-  let listen s f = Conduit_mirage.listen t s (listen f) in
+  let listen s f =
+    Conduit_mirage.listen t s (fun ip ->
+        Printf.printf "cohttp mirage server with conduit connection from %s\n"
+          (Ipaddr.V4.to_string ip);
+        listen f)
+  in
   Lwt.return listen
